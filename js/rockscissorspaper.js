5a6bc66f718dd903182c9c8a421dc53b0e8ed60c
DESKTOP-D8QNVTI\정한종 <rodwn_1@naver.com>
2019-05-19 18:33:35 +0900

    Refactor rockscissorspaper.js

--- js/rockscissorspaper.js
+++ js/rockscissorspaper.js
@@ -1,32 +1,71 @@
 let image = document.querySelector('.img'),
     buttonsElement = Array.prototype.slice.call(document.getElementsByClassName('button')),
     resetButton = document.querySelector('.reload'),
+    resultElement = document.querySelector('.result'),
     data = ['rock', 'scissors', 'paper'],
-    userData,
-    timerID;
+    convertData = {
+        rock: 0,
+        scissors: 1,
+        paper: -1
+    },
+    timerID,
+    userWinElement = document.querySelector('.userWin'),
+    computerWinElement = document.querySelector('.computerWin'),
+    drawElement = document.querySelector('.draw');
 
     let index = 0;
     function roulette () {
-
         timerID = setInterval(() => {
             if (index === data.length) {
                 index = 0;
             }
         
-            image.className = 'img' + ' ' + data[index];
+            image.className = `img ${data[index]}`;
             index++;
         }, 100);
     }
 
-    buttonsElement.forEach(item => item.addEventListener('click', (e) => {
-            clearInterval(timerID);
-            userData = e.target.getAttribute('data-hands');
-            console.log('내가 낸 값' + userData + '\n' + '컴퓨터가 낸 값' + data[index]);
-        })
-    );
-    resetButton.addEventListener('click', roulette);
+    buttonsElement.forEach(item => item.addEventListener('click', shoot));
+
+    function shoot (e) {
+        clearInterval(timerID);
+
+        let checkWin = e.target.getAttribute('data-hands') - convertData[data[index - 1]];
+        if (checkWin === -1 || checkWin === 2) {
+            resultElement.textContent = `${e.target.textContent}를 내서 이겼답니다.`;
+            userWinElement.textContent = (userWinElement.textContent = parseInt(userWinElement.textContent) + 1) || 1;
+        } else if (checkWin === 0) {
+            resultElement.textContent = `${e.target.textContent}를 내서 비겼답니다.`;
+            drawElement.textContent = (drawElement.textContent = parseInt(drawElement.textContent) + 1) || 1;
+        } else {
+            resultElement.textContent = `${e.target.textContent}를 내서 졌답니다.`;
+            computerWinElement.textContent = (computerWinElement.textContent = parseInt(computerWinElement.textContent) + 1) || 1;
+        }
+
+        buttonsElement.forEach(item => {
+            if (item === e.target) {
+                item.style.backgroundColor = '#ccc';
+                item.style.border = '2px solid #333';
+            } else {
+                item.setAttribute('disabled', '');
+            }
+            item.removeEventListener('click',shoot)
+        });
+        resetButton.style.display = 'inline-block';
+    }
+
+    resetButton.style.display = 'none';
+    resultElement.textContent = '가위? 바위? 보? 뭘 낼까요?';
+
+    resetButton.addEventListener('click', () => {
+        roulette();
+        buttonsElement.forEach(item => {
+            item.style.backgroundColor = '';
+            item.style.border = '';
+            item.removeAttribute('disabled');
+            item.addEventListener('click', shoot)
+        });
+        resultElement.textContent = '가위? 바위? 보? 뭘 낼까요?';
+        resetButton.style.display = 'none';
+    });
     roulette();
-
-
-
-
